4d2e9fc7087c556ea87eab3e4714ffc71fbde909
Jérôme Peyras <jerome.peyras@protonmail.com>
2024-12-22 15:51:30 +0100

    New party form and links added

--- party/views/__init__.py
+++ party/views/__init__.py
@@ -1,4 +1,5 @@
+from .new_party_views import page_new_party
 from .party_list_views import PartyListPage
 from .party_details_views import PartyDetailPage, PartyDetailPartial
 
-__all__ = ["PartyListPage", "PartyDetailPage", "PartyDetailPartial"]
+__all__ = ["PartyListPage", "PartyDetailPage", "PartyDetailPartial", "page_new_party"]
