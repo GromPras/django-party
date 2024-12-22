f3b5f8fbe28d37d91004a5241dcad6a002dd08bd
Jérôme Peyras <jerome.peyras@protonmail.com>
2024-12-22 13:58:09 +0100

    Add click to edit party with HTMX through crispy_forms

---
+++ party/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.forms import widgets
+
+from .models import Party
+
+
+class PartyForm(forms.ModelForm):
+    class Meta:
+        model = Party
+        fields = ("party_date", "party_time", "venue", "invitation")
+        widgets = {
+            "party_date": forms.DateInput(attrs={"type": "date"}),
+            "party_time": forms.TimeInput(attrs={"type": "time"}),
+            "invitation": forms.Textarea(attrs={"class": "w-full"}),
+        }
